7479a3534b1bf2a6fe79a94bd73241339df9a469
zpltys <zpltys@163.com>
2018-01-05 10:34:47 +0800

    change

--- sssp.cpp
+++ sssp.cpp
@@ -2,7 +2,7 @@
 int main(int argc, char* argv[])
 {
     init_workers();
-    blogel_app_sssp("/vor/usa", "/exp/sssp");
+    blogel_app_sssp("/vor/liveJournal", "/vor/sssp");
     worker_finalize();
     return 0;
 }
