120ad615cd71b375f949691f9d8348912fb33c45
zpltys <zpltys@163.com>
2018-01-19 14:31:56 +0800

    change config

--- sim.cpp
+++ sim.cpp
@@ -2,10 +2,15 @@
 // Created by zpltys on 2018/1/4.
 //
 #include "blogel_sim.h"
+#include <time.h>
 int main(int argc, char* argv[])
 {
+
+    long start = time(NULL);
     init_workers();
     blogel_app_sim("/vor/liveJournal", "/exp/sim");
     worker_finalize();
+    long end = time(NULL);
+    cout << "total cost time:" << start - end << "s" << endl;
     return 0;
 }
