301a51e4f9d001e844b32c8bcb009aa9df03c02c
zpltys <zpltys@163.com>
2018-02-02 20:39:04 +0800

    sdas

--- go.sh
+++ go.sh
@@ -6,4 +6,4 @@ make
 scp run zpltys@slave1:~/Blogel/
 scp run zpltys@slave2:~/Blogel/
 scp run zpltys@slave3:~/Blogel/
-mpiexec -n 20 -f /home/zpltys/Blogel/conf /home/zpltys/Blogel/run
+mpiexec -n 24 -f /home/zpltys/Blogel/conf /home/zpltys/Blogel/run
