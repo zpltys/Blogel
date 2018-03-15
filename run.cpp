c371f64d919a3ad57ca92d415ff0351361a8375b
zpltys <zpltys@163.com>
2018-03-15 15:40:14 +0800

    change shell

--- run.cpp
+++ run.cpp
@@ -1,12 +1,12 @@
-#include "blogel_app_pagerank1.h"
-//#include "blogel_app_pagerank2.h"
+//#include "blogel_app_pagerank1.h"
+#include "blogel_app_pagerank2.h"
 int main(int argc, char* argv[])
 {
     init_workers();
 
     long start = time(NULL);
-    blogel_app_pagerank1("/vor/liveJournal","/exp/pr1");
-    //blogel_app_pagerank2("/vor/pr1", "/exp/pr");
+    //blogel_app_pagerank1("/vor/liveJournal","/exp/pr1");
+    blogel_app_pagerank2("/vor/pr1", "/exp/pr");
     long end = time(NULL);
     cout << "total cost time:" << end - start << "s" << endl;
 
