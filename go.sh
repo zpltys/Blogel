5b5d5d2cdece67f6655bf43f3d5c46347fc05201
zpltys <zpltys@163.com>
2018-09-08 19:50:09 +0800

    change conf

--- go.sh
+++ go.sh
@@ -5,4 +5,4 @@ make
 scp run zpltys@slave1:~/Blogel/
 scp run zpltys@slave2:~/Blogel/
 scp run zpltys@slave3:~/Blogel/
-mpiexec -n 24 -f /home/zpltys/Blogel/conf /home/zpltys/Blogel/run
+mpiexec -n 24 -f /home/zpltys/Blogel/conf24 /home/zpltys/Blogel/run
