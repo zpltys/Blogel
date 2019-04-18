dfa4f2ea86bd304849a9ea78f254964db6ea4177
zpltys <gzpltys@gmail.com>
2019-04-18 17:34:52 +0800

    change go.sh

--- go.sh
+++ go.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-scp cc zpltys@slave1:~/Blogel/
-scp cc zpltys@slave3:~/Blogel/
-scp part zpltys@slave1:~/Blogel/
-scp part zpltys@slave3:~/Blogel/
-scp pr zpltys@slave1:~/Blogel/
-scp pr zpltys@slave3:~/Blogel/
+scp cc zhangshuai@mnode02:~/Blogel/
+scp cc zhangshuai@mnode03:~/Blogel/
+scp part zhangshuai@mnode02:~/Blogel/
+scp part zhangshuai@mnode03:~/Blogel/
+scp pr zhangshuai@mnode02:~/Blogel/
+scp pr zhangshuai@mnode03:~/Blogel/
