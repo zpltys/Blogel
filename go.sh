57e313aa974c2284ea962abce3e07fa7d44e1ef7
zpltys <gzpltys@gmail.com>
2019-04-19 15:28:21 +0800

    change go.sh

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 scp part zhangshuai@mnode02:~/Blogel/
 scp part zhangshuai@mnode03:~/Blogel/
-scp pr zhangshuai@mnode02:~/Blogel/
-scp pr zhangshuai@mnode03:~/Blogel/
+scp sssp zhangshuai@mnode02:~/Blogel/
+scp sssp  zhangshuai@mnode03:~/Blogel/
