5e6ad36583da4b9fe1f1566e32157f76cdbc6bf4
zpltys <gzpltys@gmail.com>
2019-05-11 12:48:15 +0800

    change part input style

--- sim_part.cpp
+++ sim_part.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[])
 {
     init_workers();
-    blogel_sim_vorPart("/pullgel/simRaw", "/vor/liveJournal");
+    blogel_sim_vorPart("/pullgel/webbase/", "/vor/liveJournal");
     worker_finalize();
     return 0;
 
