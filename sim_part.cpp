ff000544029b8c3687ac473aa2abe7a945f08879
zpltys <zpltys@163.com>
2019-07-22 20:59:04 +0800

    merge

--- sim_part.cpp
+++ sim_part.cpp
@@ -1,10 +1,13 @@
 #include "blogel_sim_vorPart.h"
-
+#include <time.h>
 int main(int argc, char* argv[])
 {
+    long start = time(NULL);
     init_workers();
     blogel_sim_vorPart("/pullgel/webbase/", "/vor/webbase");
     worker_finalize();
+    long end = time(NULL);
+    cout << "total cost time:" << end - start << "s" << endl;
     return 0;
 
 }
